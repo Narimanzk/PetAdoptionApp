9dd63904f371cfd58d16baee01b88dada0b87327
Noah-C-Z <nczwack@gmail.com>
2020-02-03 13:30:28 -0500

    Disabled tests

--- PetAdoptionApp-Backend/src/test/java/ca/mcgill/ecse321/petadoptionapp/PetAdoptionAppApplicationTests.java
+++ PetAdoptionApp-Backend/src/test/java/ca/mcgill/ecse321/petadoptionapp/PetAdoptionAppApplicationTests.java
@@ -1,12 +1,13 @@
 package ca.mcgill.ecse321.petadoptionapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class PetAdoptionAppApplicationTests {
 
-	@Ignore
 	@Test
 	void contextLoads() {
 	}
