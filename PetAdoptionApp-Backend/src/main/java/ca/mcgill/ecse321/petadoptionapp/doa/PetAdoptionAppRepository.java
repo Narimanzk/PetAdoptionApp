cee6b208eef3ce19b69a3ff308a788d8c31574cc
shubymao <le-li.mao@mail.mcgill.ca>
2020-02-06 00:23:31 -0500

    added repository in the backend system

---
+++ PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/doa/PetAdoptionAppRepository.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse321.petadoptionapp.doa;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.petadoptionapp.model.RegularUser;
+
+@Repository
+public class PetAdoptionAppRepository {
+
+		@Autowired
+		EntityManager entityManager;
+
+		@Transactional
+		public RegularUser createRegularUser(String username, String password, String email) {
+			return null;
+		}
+
+		@Transactional
+		public RegularUser getPerson(String username) {
+			return null;
+		}
+}
