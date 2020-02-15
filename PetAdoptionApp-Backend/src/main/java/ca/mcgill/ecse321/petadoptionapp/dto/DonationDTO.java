67f5b024e8f1c800fd5c911e4f8ce8efe8727c80
Nariman <nariman.zendehrooh@gmail.com>
2020-02-15 16:12:57 -0500

    Added DTO for address and donation

---
+++ PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/dto/DonationDTO.java
@@ -0,0 +1,55 @@
+package ca.mcgill.ecse321.petadoptionapp.dto;
+
+public class DonationDTO {
+	private int amount;
+	private PetShelterDTO donatedTo;
+	private RegularUserDTO donatedFrom;
+
+
+	public DonationDTO() {
+
+	}
+
+
+	public DonationDTO(int amount, PetShelterDTO donatedTo, RegularUserDTO donatedFrom) {
+		this.amount = amount;
+		this.donatedTo = donatedTo;
+		this.donatedFrom = donatedFrom;
+	}
+
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
+	public PetShelterDTO getDonatedTo() {
+		return donatedTo;
+	}
+
+
+	public void setDonatedTo(PetShelterDTO donatedTo) {
+		this.donatedTo = donatedTo;
+	}
+
+
+	public RegularUserDTO getDonatedFrom() {
+		return donatedFrom;
+	}
+
+
+	public void setDonatedFrom(RegularUserDTO donatedFrom) {
+		this.donatedFrom = donatedFrom;
+	}
+
+
+
+
+
+}
