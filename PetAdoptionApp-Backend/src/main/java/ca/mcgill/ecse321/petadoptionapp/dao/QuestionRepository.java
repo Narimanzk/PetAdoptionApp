93a490531baa969ec8efd485d481d430092ab07a
lechristy25 <lenoy.christy@mail.mcgill.ca>
2020-02-24 19:33:18 -0500

    Added Service Methods for Question Class

--- PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/dao/QuestionRepository.java
+++ PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/dao/QuestionRepository.java
@@ -1,12 +1,18 @@
 package ca.mcgill.ecse321.petadoptionapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import ca.mcgill.ecse321.petadoptionapp.model.GeneralUser;
 import ca.mcgill.ecse321.petadoptionapp.model.Question;
+import ca.mcgill.ecse321.petadoptionapp.model.Response;
 
 public interface QuestionRepository extends CrudRepository <Question, Integer> {
 	
 	Question findQuestionById(Integer id);
+	Question findQuestionByResponse(Response response);
+	List<Question> findQuestionsByUser(GeneralUser user);
 	
 
 }
