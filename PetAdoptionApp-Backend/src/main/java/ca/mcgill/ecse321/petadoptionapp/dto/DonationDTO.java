9e49b38e5c77bf658642cadf7cadb1121576f4e0
Nariman <nariman.zendehrooh@gmail.com>
2020-02-28 04:44:02 -0500

    Update PetAdoptionAppController.java

--- PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/dto/DonationDTO.java
+++ PetAdoptionApp-Backend/src/main/java/ca/mcgill/ecse321/petadoptionapp/dto/DonationDTO.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse321.petadoptionapp.dto;
 
 public class DonationDTO {
+	private Integer id;
 	private int amount;
 	private GeneralUserDTO donatedTo;
 	private GeneralUserDTO donatedFrom;
@@ -11,8 +12,18 @@ public class DonationDTO {
 	}
 
 	
-	public DonationDTO(int amount) {
+	public DonationDTO(Integer id, int amount) {
+		this.id = id;
 		this.amount = amount;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 
