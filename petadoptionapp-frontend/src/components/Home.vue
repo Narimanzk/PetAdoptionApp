a7d9c96e1864a698f72a862e7fc9fd055211194a
andrewta999 <andrewta365@gmail.com>
2020-03-15 19:24:20 -0400

    init frontend code

---
+++ petadoptionapp-frontend/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    {{message}}
+  </div>
+</template>
+
+<script>
+//scripts go here
+export default {
+    name: "Home", 
+    data(){
+        return {
+            message: "Hello World"
+        }
+    }
+}
+</script>
+
+<style>
+/**define styles here */
+</style>
